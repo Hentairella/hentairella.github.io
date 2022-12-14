7a91f3294a7db11ae7bf7b8ddc1f5c85306cfefd
Hentairella <75986863+Hentairella@users.noreply.github.com>
2022-12-14 12:40:42 -0500

    Update BOT_GhostChasing.js

--- BOT_GhostChasing.js
+++ BOT_GhostChasing.js
@@ -1,1 +1,9 @@
 
+Player.Description = `Code available here: https://github.com/keykey5/BC-BOT-repository
+Comment and suggestion thread on BC Discord: https://discord.com/channels/554377975714414605/1024007221845164052
+----------------------------------------
+This little kinky game needs two players. When the game start you will be able to make actions. 
+Use emote <*> to play.
+` // end of description
+ServerSend("AccountUpdate", { Description: Player.Description });
+ChatRoomCharacterUpdate(Player)
