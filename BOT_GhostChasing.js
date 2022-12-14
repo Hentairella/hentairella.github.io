d419d91afe2aea344c75378536df8ea83a52f835
Hentairella <75986863+Hentairella@users.noreply.github.com>
2022-12-14 13:10:15 -0500

    Update BOT_GhostChasing.js

--- BOT_GhostChasing.js
+++ BOT_GhostChasing.js
@@ -1,7 +1,9 @@
 
 Player.Description = `Code available here: https://github.com/Hentairella/hentairella.github.io
 ----------------------------------------
-This little kinky game needs two players. When the game start you will be able to make actions. 
+This game has players trying to find and capture a ghost before dawn.
+
+If they fail, the players will be sexually teased until they are released at dawn.
 Use emote <*> to play.
 ` // end of description
 ServerSend("AccountUpdate", { Description: Player.Description });
